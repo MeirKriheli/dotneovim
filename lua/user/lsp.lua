4cbc9e81bb22be49a82e835265e12b2f37385c2d
Meir Kriheli <mkriheli@gmail.com>
2022-11-15 16:29:11 +0200

    Removed comment

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -112,25 +112,6 @@ require("mason-lspconfig").setup_handlers({
 })
 
 require("mason-lspconfig").setup()
--- lsp-install
--- local lsp_installer = require("nvim-lsp-installer")
-
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
--- lsp_installer.on_server_ready(function(server)
---     local opts = make_config()
---
---     -- load custom lsp server settings
--- 	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp_settings." .. server.name)
--- 	if has_custom_opts then
--- 	 	opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
--- 	end
---
---     -- This setup() function is exactly the same as lspconfig's setup function.
---     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
---     server:setup(opts)
--- end)
-
 require("nvim-gps").setup()
 require "lsp_signature".setup()
 require("null-ls").setup()
