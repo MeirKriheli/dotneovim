8a02573e5d69a42e31bec5ac2217d8b0211125a9
Meir Kriheli <mkriheli@gmail.com>
2025-04-20 15:52:53 +0300

    Added buffer and AI key groups

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,11 +5,13 @@ return {
         preset = "helix",
         spec = {
             { "<leader>f", group = "Find" },
+            { "<leader>b", group = "Buffer" },
             { "<leader>c", group = "Code" },
             { "<leader>g", group = "Git" },
             { "<leader>gh", group = "Git [H]unk" },
             { "<leader>gt", group = "Git [T]oggle" },
             { "<leader>u", group = "Toggle [U]I elements" },
+            { "<leader>a", group = "AI" },
         },
     },
     keys = {
