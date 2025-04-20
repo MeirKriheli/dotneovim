eb3b0e09f415162ae11be289ee22fa78b1ec6670
Meir Kriheli <mkriheli@gmail.com>
2025-04-20 15:53:45 +0300

    Added bashls to lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -242,7 +242,7 @@ return {
             -- But for many setups, the LSP (`ts_ls`) will work just fine
             -- ts_ls = {},
             --
-
+            bashls = {},
             lua_ls = {
                 -- cmd = { ... },
                 -- filetypes = { ... },
