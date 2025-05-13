69d422f81ead56deeb4dca3dfd0660bb028e29e7
Meir Kriheli <mkriheli@gmail.com>
2025-05-13 13:23:45 +0300

    Added ctrl+q for fzflua to quickfix

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -4,7 +4,13 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     -- or if using mini.icons/mini.nvim
     -- dependencies = { "echasnovski/mini.icons" },
-    opts = {},
+    opts = {
+        keymap = {
+            fzf = {
+                ["ctrl-q"] = "select-all+accept",
+            },
+        },
+    },
     keys = {
         {
             "<leader>ff",
