79e21fce235b8b78017f38de34c0c36d0f13389d
Meir Kriheli <mkriheli@gmail.com>
2025-05-13 13:24:26 +0300

    Formatted kanagawa.lua and disabled tranparency

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,13 +1,13 @@
 return {
-	"rebelot/kanagawa.nvim",
-	config = function()
-		require("kanagawa").setup({
-			compile=true,
-			transparent=true,
-		});
-		vim.cmd("colorscheme kanagawa");
-	end,
-	build = function()
-		vim.cmd("KanagawaCompile");
-	end,
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            compile = true,
+            -- transparent=true,
+        })
+        vim.cmd("colorscheme kanagawa")
+    end,
+    build = function()
+        vim.cmd("KanagawaCompile")
+    end,
 }
