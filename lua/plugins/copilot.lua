a2c6cad93ef845ad293526c0c2a2cf2154cc52f5
Meir Kriheli <mkriheli@gmail.com>
2025-05-13 13:26:15 +0300

    Added keymap for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,11 +8,11 @@ return {
             enabled = not vim.g.ai_cmp,
             auto_trigger = true,
             hide_during_completion = vim.g.ai_cmp,
-            keymap = {
-                accept = false, -- handled by nvim-cmp / blink.cmp
-                next = "<M-]>",
-                prev = "<M-[>",
-            },
+            -- keymap = {
+            --     accept = false, -- handled by nvim-cmp / blink.cmp
+            --     next = "<M-]>",
+            --     prev = "<M-[>",
+            -- },
         },
         panel = { enabled = false },
         filetypes = {
