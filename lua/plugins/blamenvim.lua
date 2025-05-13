7e7c3f76a063fce51db7b818a698109dbab61f90
Meir Kriheli <mkriheli@gmail.com>
2025-05-13 15:57:48 +0300

    Added blame.nvim

---
+++ lua/plugins/blamenvim.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "FabijanZulj/blame.nvim",
+        lazy = false,
+        opts = {},
+        keys = {
+            {
+                "<leader>gb",
+                "<cmd>BlameToggle window<CR>",
+                desc = "Toggle Git Blame",
+            },
+        },
+    },
+}
